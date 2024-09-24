861bdf6f50cd63ed076605756e6b93b718886261
Kevin Wu <kevinwu098@gmail.com>
2024-09-24 14:00:54 -0700

    chore: update prettier

---
+++ client/prettier.config.cjs
@@ -0,0 +1,39 @@
+// @ts-check
+
+/**
+ * @type{import('prettier').Config}
+ */
+const config = {
+    tabWidth: 4,
+    printWidth: 80,
+    proseWrap: "always",
+    trailingComma: "es5",
+    bracketSpacing: true,
+    arrowParens: "always",
+    singleAttributePerLine: true,
+    endOfLine: "lf",
+    singleQuote: false,
+    semi: true,
+    useTabs: false,
+    plugins: ["@ianvs/prettier-plugin-sort-imports"],
+    importOrder: [
+        "^(react/(.*)$)|^(react$)",
+        "^(next/(.*)$)|^(next$)",
+        "<THIRD_PARTY_MODULES>",
+        "",
+        "^types$",
+        "^~/types/(.*)$",
+        "^~/config/(.*)$",
+        "^~/lib/(.*)$",
+        "^~/hooks/(.*)$",
+        "^~/components/ui/(.*)$",
+        "^~/components/(.*)$",
+        "^~/styles/(.*)$",
+        "^~/app/(.*)$",
+        "",
+        "^[./]",
+    ],
+    importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+};
+
+module.exports = config;
