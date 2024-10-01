1c9d98e8115779b57b7ebe3f06f9d656dce48c2b
Kevin Wu <kevinwu098@gmail.com>
2024-10-01 04:02:41 -0400

    style: scaffold select

--- client/src/app/playground/page.tsx
+++ client/src/app/playground/page.tsx
@@ -1,11 +1,47 @@
 "use client";
 
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/dispatch/select";
+
 import { Buttons } from "./components/buttons";
 import { Toasts } from "./components/toasts";
 
 export default function page() {
     return (
         <div className="flex h-[100dvh] w-[100dvw] flex-col space-y-2 bg-[#111111] p-4">
+            <div className="uppercase">
+                <Select defaultValue="SF">
+                    <SelectTrigger>
+                        <SelectValue placeholder="LOCATION" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem
+                            value="SF"
+                            className="uppercase"
+                        >
+                            San Francisco, CA
+                        </SelectItem>
+                        <SelectItem
+                            value="BER"
+                            disabled
+                        >
+                            Berkeley, CA
+                        </SelectItem>
+                        <SelectItem
+                            value="OAK"
+                            disabled
+                        >
+                            Oakland, CA
+                        </SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
+
             <Toasts />
 
             <Buttons />
