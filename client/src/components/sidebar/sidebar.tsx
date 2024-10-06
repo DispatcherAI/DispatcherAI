f342ec62dffe2f465d3aaeb8b9799e4f48878e05
Kevin Wu <kevinwu098@gmail.com>
2024-10-06 02:17:47 -0700

    feat: add separator

--- client/src/components/sidebar/sidebar.tsx
+++ client/src/components/sidebar/sidebar.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React, { useCallback, useState } from "react";
+import { Separator } from "@/components/dispatch/separator";
 import { cn } from "@/lib/utils";
 
-import { Separator } from "../ui/separator";
 import { SidebarAvatar } from "./sidebar-avatar";
 import { SidebarNav } from "./sidebar-nav";
 import { SidebarToggle } from "./sidebar-toggle";
@@ -26,7 +26,7 @@ export function Sidebar({ className }: SidebarProps) {
     return (
         <nav
             className={cn(
-                `border-dp-outlineNotSelected relative z-50 hidden h-screen border-r-2 bg-dp-background px-2 pb-4 pt-3 md:block`,
+                `relative z-50 hidden h-screen border-r-2 border-dp-outlineNotSelected bg-dp-background px-2 pb-4 pt-3 md:block`,
                 status && "duration-500",
                 isOpen ? "w-52 min-w-52" : "w-[50px] min-w-[50px]",
                 className
@@ -37,11 +37,11 @@ export function Sidebar({ className }: SidebarProps) {
                 handleToggle={handleToggle}
             />
 
-            <Separator className="bg-dp-outlineNotSelected mt-2 h-[1px]" />
+            <Separator className="mt-2 h-[1px]" />
 
             <SidebarAvatar isOpen={isOpen} />
 
-            <Separator className="bg-dp-outlineNotSelected h-[1px]" />
+            <Separator className="h-[1px]" />
 
             <SidebarNav />
         </nav>
