5a1bb734ee6dd657f7a10fa5651a96b118c236fd
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2024-10-23 15:41:17 -0700

    Manual revert

--- client/src/app/playground/page.tsx
+++ client/src/app/playground/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { Buttons } from "./components/buttons";
-import { Checkboxes } from "./components/checkboxes";
-import { Labels } from "./components/labels";
 import { Selects } from "./components/selects";
 import { Tabs } from "./components/tabs";
 import { Toasts } from "./components/toasts";
@@ -10,8 +8,6 @@ import { Toasts } from "./components/toasts";
 export default function page() {
     return (
         <div className="flex h-[100dvh] w-[100dvw] flex-col space-y-2 bg-[#111111] p-4">
-            <Checkboxes />
-            <Labels />
             <Tabs />
             <Selects />
             <Toasts />
